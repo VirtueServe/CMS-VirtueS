25f908933350350717fadddc511f15c8078d9c59
Vinay <VKasuladevi@virtueserve.com>
2026-04-01 22:40:24 +0530

    uncommented ssl

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      // ssl: { rejectUnauthorized: false },
+      ssl: env.bool('DATABASE_SSL', false) ? { rejectUnauthorized: false } : false,
     },
     pool: {
       min: env.int('DATABASE_POOL_MIN', 2),
